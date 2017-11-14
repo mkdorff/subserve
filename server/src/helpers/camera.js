d3b41c098e5a357ed46768ae42962910311fcee9
mkdorff <mkdorff@outlook.com>
2017-11-14 14:35:22 -0700

    Added README and barebones of camera helper.

---
+++ server/src/helpers/camera.js
@@ -0,0 +1,15 @@
+// var v4l2camera = require("v4l2camera");
+import v4l2camera from 'v4l2camera'
+const cam = new v4l2camera.Camera("/dev/video0");
+
+cam.configSet({width: 352, height: 288}); // Try different sizes
+cam.start();
+cam.capture(function loop() {
+  cam.capture(loop);
+});
+
+// Maybe do a socketio con - 
+// also convert yuyv to rgba here. Send raw rgab data through sockets.
+
+
+export { cam }
