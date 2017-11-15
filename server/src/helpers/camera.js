49bb25d987e8178fcec97d1f968ce024fe5be0ae
mkdorff <mkdorff@outlook.com>
2017-11-15 16:33:29 -0700

    a few more things to try on the pi

--- server/src/helpers/camera.js
+++ server/src/helpers/camera.js
@@ -1,9 +1,18 @@
-var v4l2camera = require("v4l2camera");
-import v4l2camera2 from 'v4l2camera'
 
-console.log(v4l2camera)
-console.log(v4l2camera2)
-// const cam = new v4l2camera.Camera("/dev/video0");
+// We can't load this other than on a Pi enviroment so we'll need checks
+let v4l2camera = null;
+let cam;
+try {
+  v4l2camera = require("v4l2camera");
+  cam = new v4l2camera.Camera("/dev/video0");
+} catch (err) {
+  console.log("The camera module only works in a Pi Enviroment")
+}
+
+console.log(!!v4l2camera)
+console.log(cam);
+
+
 
 // cam.configSet({width: 352, height: 288}); // Try different sizes
 // cam.start();
