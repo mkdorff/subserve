fdbecd55bcdc52fc837d42c32cb68d7b419742be
mkdorff <mkdorff@outlook.com>
2017-11-17 13:49:50 -0700

    tweaks

--- client/src/services/videoStream.js
+++ client/src/services/videoStream.js
@@ -16,7 +16,7 @@ export function paintCanvas(canvas) {
       const rgbaRaw = await yuyv2rgba(yuyvRaw, new Uint8ClampedArray(405504), WIDTH, HEIGHT);
       const imageData = new ImageData(rgbaRaw, WIDTH, HEIGHT);
       c2d.putImageData(imageData, 0, 0);
-      setTimeout(load, 100);
+      setTimeout(load, 50);
     }, false);
     req.open("GET", "/video-feed", true);
     req.send();
